866fb29f42612aa7d657a0afc47f9e648fb4db44
MarcusVFRibeiro <viniciusxz12@outlook.com>
2024-11-18 09:45:25 -0300

    Solução vetor dinâmico

--- main.c
+++ main.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
+#include<stdlib.h>
 
-int main(){
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  int *p = NULL;
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
+  if(n == 0){
+    printf("[vazio]\n");
     return 0;
+  }
+    
+  p = (int*)malloc(sizeof(int)*n);
+
+  for(int i  = 0; i < n; i++)
+  {
+    scanf("%d", &p[i]);
+  }
+
+    printf("[");
+
+    for(int i = 0; i < n; i++)
+  {
+    if(i != n-1)
+    {
+      printf("%d,",p[i]);
+    }
+    else
+    {
+      printf("%d]\n", p[i]);
+    }
+  }
+  
+  free(p);
+
+  return 0;
 }
